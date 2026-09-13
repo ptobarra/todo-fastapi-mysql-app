d6bf1ce0285190f9dae756f31b7d4fcbb87d917a
ptobarra <12190195+ptobarra@users.noreply.github.com>
2026-09-13 23:51:13 +0200

    database.py updated with a .env environment variable that will use a value when deployed locally and another value when deployed in the render.com service

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -13,12 +13,12 @@ from sqlalchemy.orm import sessionmaker
 #     "postgresql://postgres:test1234!@localhost:5434/TodoApplicationDatabase"
 # )
 
-# TODO: uncomment the 3 lines below to run the server locally
+# TODO: uncomment the 3 lines below to run the server locally (value also stored in the .env local variable)
 # SQLALCHEMY_DATABASE_URL = (
 #     "mysql+pymysql://root:test1234@localhost:3306/todoapplicationdatabase"
 # )
 
-# TODO: the 2 lines below are to run the server onn the render.com service
+# TODO: the 2 lines below are to run the server on the render.com service
 load_dotenv()  # loads .env locally; on Render, env vars are already set in the dashboard
 SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
 
